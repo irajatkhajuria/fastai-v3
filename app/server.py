5af31d30bb59d2be1fe223a831d9282f743e8776
Rajat Khajuria <rjtkhajuria@gmail.com>
2019-06-14 19:56:10 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1Zkk7baTC2hnwpY0Cjs_CUE3VVTRBwsSv'
+export_file_url = 'https://www.dropbox.com/s/a3p8rz8m8ief9dy/model-rajat.pkl?dl=1 '
 classes = ['Pneumonia','Normal']
 path = Path(__file__).parent
 
